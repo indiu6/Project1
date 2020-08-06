8b88390d9b626ddee98eb977c6a71c85a4316440
indiu6 <hewas6@gmail.com>
2020-08-06 00:38:07 -0400

    msg

--- routers/basic.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let basicCalRouter = express.Router();
-
-basicCalRouter.get('/', function(req, res) { 
-    res.render('basicCal')   
-});
-
-module.exports = basicCalRouter;
